bc54822e889324c930e64b827ac4019eaa298bab
ismailyou <jadid.ismail.tdi@gmail.com>
2020-09-17 11:52:07 +0100

    write the message flush, the create question item

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('/questions', App\Http\Controllers\QuestionsController::class);
