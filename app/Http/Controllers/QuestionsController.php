7f2836e09ffe2622fe2daeddeb229c6f663948d8
ismailyou <jadid.ismail.tdi@gmail.com>
2020-09-18 01:18:09 +0100

    Implimenting Authorization using policies

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class QuestionsController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -62,6 +67,7 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
+        $this->authorize('update',$question);
         return view('questions.edit', compact('question'));
     }
 
@@ -74,6 +80,7 @@ class QuestionsController extends Controller
      */
     public function update(AddQuestionValidationRequest $request, Question $question)
     {
+        $this->authorize('update',$question);
         $question->update($request->only('title', 'body'));
         return redirect()->route('questions.index')->with('success', 'Your Question has been updated !');
     }
@@ -86,6 +93,7 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
+        $this->authorize('delete',$question);
         $question->delete();
         return redirect('/questions')->with('success', 'Your Question has been removed');
     }
