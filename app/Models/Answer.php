48b88edd164112809c0f7e0ad8d5a1765016d074
ismailyou <jadid.ismail.tdi@gmail.com>
2020-09-22 19:37:08 +0100

    dipaly answers for question

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -30,4 +30,16 @@ class Answer extends Model
        });
 
     }
+
+    public function getBodyHtmlAttribute()
+    {
+        return $this->body;
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+
+
 }
